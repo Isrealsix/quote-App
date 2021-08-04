6f10877709671130653585b8bf05944c4937ec26
Isreal6ix <isrealsix@gmail.com>
2021-08-04 19:58:04 +0700

    added Various Routes and Dynamic Routes

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import { useParams, Route } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
+function QuoteDetail() {
+	const params = useParams();
+	return (
+		<Fragment>
+			<h1>Quote Detail Page</h1>
+			<p>{params.quoteId}</p>
+			<Route path="/quotes/:quoteId/comments">
+				<Comments />
+			</Route>
+		</Fragment>
+	);
+}
+
+export default QuoteDetail;
