eff57a27cf2dc889229b7cccde97fc4ceb95d1f0
Isreal6ix <isrealsix@gmail.com>
2021-08-04 20:21:54 +0700

    Nested the routes inside the Layout Compponent

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -3,11 +3,12 @@ import { useParams, Route } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 function QuoteDetail() {
 	const params = useParams();
+	const quoteId = params.quoteId;
 	return (
 		<Fragment>
 			<h1>Quote Detail Page</h1>
 			<p>{params.quoteId}</p>
-			<Route path="/quotes/:quoteId/comments">
+			<Route path={`/quotes/${quoteId}/comments`}>
 				<Comments />
 			</Route>
 		</Fragment>
