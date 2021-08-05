fddef350edab867ddc0d02c6107b57b3aaa2755e
Isreal6ix <isrealsix@gmail.com>
2021-08-05 12:38:25 +0700

    used the custom component to fetch all Quote on the AllQuotes component

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,15 +1,39 @@
+import { useEffect } from 'react';
 import QuoteList from '../components/quotes/QuoteList';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
-const DUMMY_QUOTES = [
-	{ id: 'go_q1', author: 'Vagitus', text: 'A child is Born' },
-	{
-		id: 'go_q7',
-		author: 'Steve',
-		text: 'Make a difference!',
-	},
-];
 function AllQuotes() {
-	return <QuoteList quotes={DUMMY_QUOTES} />;
+	const {
+		sendRequest,
+		status,
+		data: loadedQuotes,
+		error,
+	} = useHttp(getAllQuotes, true);
+
+	useEffect(() => {
+		sendRequest();
+	}, [sendRequest]);
+
+	if (status === 'pending') {
+		return (
+			<div className="centered">
+				<LoadingSpinner />
+			</div>
+		);
+	}
+
+	if (error) {
+		return <p className="centered focused">{error}</p>;
+	}
+
+	if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+		return <NoQuotesFound />;
+	}
+
+	return <QuoteList quotes={loadedQuotes} />;
 }
 
 export default AllQuotes;
